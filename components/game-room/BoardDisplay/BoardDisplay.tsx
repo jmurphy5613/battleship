38c87ebdbdb6356dc282ff2d52c01ba531729f8e
jmurphy5613 <jmurphy5613@gmail.com>
2022-11-25 13:52:27 -0500

    added key prop

--- components/game-room/BoardDisplay/BoardDisplay.tsx
+++ components/game-room/BoardDisplay/BoardDisplay.tsx
@@ -10,8 +10,7 @@ const BoardDisplay:React.FC<BoardDisplayProps> = ({ board, handleMove }) => {
         <div className={styles.grid}>
             {board.map((element:string, index:number) => {
                 return (
-                    <div onClick={() => {
-                        console.log(index)
+                    <div key={index} onClick={() => {
                         handleMove(index, board)
                     }} className={styles["grid-item"]}>
                         <h1 className={styles.label}>{element}</h1>
