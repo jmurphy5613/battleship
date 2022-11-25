170083bd4b7082c63075a1a39daca56b0bcb4ba2
jmurphy5613 <jmurphy5613@gmail.com>
2022-11-25 13:44:02 -0500

    got moves working with live updating without changing moves

--- components/game-room/BoardDisplay/BoardDisplay.tsx
+++ components/game-room/BoardDisplay/BoardDisplay.tsx
@@ -1,15 +1,19 @@
 import styles from './BoardDisplay.module.css'
 
 type BoardDisplayProps = {
-    board: Array<string>
+    board: Array<string>,
+    handleMove: (index:number, board:Array<string>) => void
 }
 
-const BoardDisplay:React.FC<BoardDisplayProps> = ({ board }) => {
+const BoardDisplay:React.FC<BoardDisplayProps> = ({ board, handleMove }) => {
     return (
         <div className={styles.grid}>
             {board.map((element:string, index:number) => {
                 return (
-                    <div className={styles["grid-item"]}>
+                    <div onClick={() => {
+                        console.log(index)
+                        handleMove(index, board)
+                    }} className={styles["grid-item"]}>
                         <h1 className={styles.label}>{element}</h1>
                     </div>
                 )
--- pages/game/[gameId].tsx
+++ pages/game/[gameId].tsx
@@ -1,6 +1,6 @@
 import styles from '../../styles/Game.module.css'
 import { useRouter } from 'next/router'
-import { useQuery } from '../../convex/_generated/react'
+import { useMutation, useQuery } from '../../convex/_generated/react'
 import { useEffect, useState } from 'react'
 import BoardDisplay from '../../components/game-room/BoardDisplay/BoardDisplay'
 import PlayerTitles from '../../components/game-room/PlayerTitles/PlayerTitles'
@@ -13,10 +13,46 @@ const Game = () => {
     const [playerName, setPlayerName] = useState<string>('')
 
     const game = useQuery("gameByCode", code)
+    const editGame = useMutation("editGame")
+
+    const isPlayerOneOrTwo = () => {
+        if(!game) return
+        if(game[0].playerOne === playerName) {
+            return 1
+        } 
+        return 2
+    }
+
+    const getNewTurn = () => {
+        if(!game) return
+        
+    }
 
     const isCurrentUsersTurn = (player:string) => {
         if(!game) return false
         return player === game[0].currentTurn
+    }
+
+    const getCurrentUsersBoard = () => {
+        if(!game) return
+        if(playerName === game[0].playerOne) return game[0].playerOneBoard
+        else if(playerName === game[0].playerTwo) return game[0].playerTwoBoard
+        return
+    }
+
+    const handleMove = (index:number, board:Array<string>) => {
+        if(!game) return
+        //check if the board that is being acted on is the users board
+        if(isCurrentUsersTurn(playerName) && getCurrentUsersBoard() === board) {
+            const currentBoard = board
+            currentBoard[index] = 'x'
+            if(isPlayerOneOrTwo() === 1) {
+                editGame(game[0].playerOne, game[0].playerTwo, game[0].currentTurn, currentBoard, game[0].playerTwoBoard, game[0].isOver, code, game[0]._id)
+            }
+            else {
+                editGame(game[0].playerOne, game[0].playerTwo, game[0].currentTurn, game[0].playerOneBoard, currentBoard, game[0].isOver, code, game[0]._id)
+            }
+        }
     }
     
     useEffect(() => {
@@ -37,11 +73,11 @@ const Game = () => {
             <div className={styles.boards}>
                 <div className={styles.left}>
                     <PlayerTitles playerName={game[0].playerOne} userName={playerName} isTurn={isCurrentUsersTurn(game[0].playerOne)}  />
-                    <BoardDisplay board={game[0].playerOneBoard}  />
+                    <BoardDisplay handleMove={handleMove} board={game[0].playerOneBoard}  />
                 </div>
                 <div className={styles.right}>
                     <PlayerTitles playerName={game[0].playerTwo} userName={playerName} isTurn={isCurrentUsersTurn(game[0].playerTwo)} />
-                    <BoardDisplay board={game[0].playerTwoBoard} />
+                    <BoardDisplay handleMove={handleMove} board={game[0].playerTwoBoard} />
                 </div>
             </div>
         </div>
